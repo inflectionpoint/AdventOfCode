60c9358abfe2bcc39f38e8e95e03977374992743
InflectionPoint <andrew.kamensky@gmail.com>
2021-12-21 18:34:52 -0500

    Added Day 8 Test Cases

---
+++ 2021Tests/TestDay08.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using Y2021;
+
+namespace TestSampleData
+{
+    public class TestDay08
+    {
+        [Fact]
+        public void ValidatePartA()
+        {
+            //Arrange
+            Day08 D08 = new();
+            (D08.InputSignals, D08.OutputSignals) = DataLoader.Day08Load(true);
+
+            //Act
+            int result = D08.CountOutputDigits();
+
+            //Assert
+            Assert.Equal(26, result);
+        }
+
+        [Fact]
+        public void ValidatePartB()
+        {
+            //Arrange
+            Day08 D08 = new();
+            (D08.InputSignals, D08.OutputSignals) = DataLoader.Day08Load(true);
+
+            //Act
+            int result = D08.SumDecodedOutputs();
+
+            //Assert
+            Assert.Equal(61229, result);
+        }
+    }
+}
