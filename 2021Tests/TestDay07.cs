504db5393fcae785c1ebf950a7b19e100a578382
InflectionPoint <andrew.kamensky@gmail.com>
2021-12-21 18:31:22 -0500

    Added Day 7 Test Case

---
+++ 2021Tests/TestDay07.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using Y2021;
+
+namespace TestSampleData
+{
+    public class TestDay07
+    {
+        [Fact]
+        public void ValidatePartA()
+        {
+            //Arrange
+            Day07 D07 = new();
+            D07.HorizontalPositions = DataLoader.Day07Sample();
+
+            //Act
+            int result = D07.ComputeFuelConsumptionConst();
+
+            //Assert
+            Assert.Equal(37, result);
+        }
+
+        [Fact]
+        public void ValidatePartB()
+        {
+            //Arrange
+            Day07 D07 = new();
+            D07.HorizontalPositions = DataLoader.Day07Sample();
+
+            //Act
+            int result = D07.ComputeFuelConsumptionLinear();
+
+            //Assert
+            Assert.Equal(168, result);
+        }
+    }
+}
